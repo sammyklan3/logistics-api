c08a91575df55d16d091312a797a342fa1776a72
sammyklan3 <sammyklane7@gmail.com>
2024-08-02 17:37:43 +0300

    Adding database config into its own file

--- models/Shipper.js
+++ models/Shipper.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/config");
+const sequelize = require("../config/database");
 const User = require("./User");
 
 const Shipper = sequelize.define(
