5014304fbd32955c6de2124038c3d54a7c602f6c
sammyklan3 <sammyklane7@gmail.com>
2024-09-16 13:05:19 +0300

    Add route for deleting a job

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const authenticateToken = require("../middleware/authMiddleware");
-const { createJob, updateJob } = require("../controllers/jobController");
+const { createJob, updateJob, deleteJob } = require("../controllers/jobController");
 const router = express.Router();
 
 router.post("/create", authenticateToken, createJob);
 router.patch("/update/:id", authenticateToken, updateJob);
+router.delete("/delete/:id", authenticateToken, deleteJob);
 
 module.exports = router;
